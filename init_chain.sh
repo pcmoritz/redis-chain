0e467586135392b4552fa5c86ef10f42f917e9d5
Philipp Moritz <pcmoritz@gmail.com>
2017-11-15 10:56:43 -0800

    add sleep

--- init_chain.sh
+++ init_chain.sh
@@ -8,6 +8,8 @@ make -j
 ./redis/src/redis-server --loadmodule libredis_chain_async.so --port 6380 &> 6380.log &
 ./redis/src/redis-server --loadmodule libredis_chain_async.so --port 6381 &> 6381.log &
 
+sleep 1
+
 ./redis/src/redis-cli -p 6379 chain.initialize head 0.0.0.0 6381 0.0.0.0 6380
 ./redis/src/redis-cli -p 6380 chain.initialize middle 0.0.0.0 6379 0.0.0.0 6381
 ./redis/src/redis-cli -p 6381 chain.initialize tail 0.0.0.0 6380 0.0.0.0 6379
