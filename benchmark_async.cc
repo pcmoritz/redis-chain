4d4a6143b0ca2e964eacf20d7ddba3503ef3f200
Philipp Moritz <pcmoritz@gmail.com>
2017-11-16 20:11:36 -0800

    update

--- benchmark_async.cc
+++ benchmark_async.cc
@@ -14,6 +14,7 @@ extern "C" {
 #include <vector>
 
 static aeEventLoop *loop;
+redisAsyncContext *client;
 
 int64_t now() {
   return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
@@ -43,17 +44,27 @@ void SubscriptionCallback(redisAsyncContext* c, void* r, void* privdata) {
   }
   int64_t t = reinterpret_cast<int64_t>(privdata);
   timestamps2.push_back(now() - t);
+  if (timestamps2.size() == 1) {
+    for (int i = 0; i < 3; ++i) {
+      redisAsyncCommand(client, Callback, reinterpret_cast<void*>(now()), "CHAIN.PUT hello world");
+    }
+  }
   if(timestamps2.size() == 3) {
     aeStop(loop);
   }
 }
 
+void connectCallback(const redisAsyncContext *c, int status) {
+  printf("connected\n");
+  T = now();
+}
+
 int main() {
   // need to set CONFIG SET protected-mode no
-  redisAsyncContext *c = redisAsyncConnect("172.31.18.230", 6379);
-  if (c->err) {
+  client = redisAsyncConnect("172.31.18.230", 6379);
+  if (client->err) {
     /* Let *c leak for now... */
-    printf("Error: %s\n", c->errstr);
+    printf("Error: %s\n", client->errstr);
     return 1;
   }
   // IMPORTANT: need a separate context for subscribers
@@ -64,11 +75,9 @@ int main() {
     return 1;
   }
   loop = aeCreateEventLoop(64);
-  redisAeAttach(loop, c);
+  redisAeAttach(loop, client);
+  redisAsyncSetConnectCallback(client,connectCallback);
   redisAeAttach(loop, s);
-  for (int i = 0; i < 3; ++i) {
-    redisAsyncCommand(c, Callback, reinterpret_cast<void*>(now()), "CHAIN.PUT hello world");
-  }
   redisAsyncCommand(s, SubscriptionCallback, reinterpret_cast<void*>(now()), "SUBSCRIBE answers");
   aeMain(loop);
   for (const int64_t& t : timestamps1) {
